fefadf53c4481477563df62acb9d7d74bc8952c5
Khushi Sahoo <khushisahoo@Khushis-MacBook-Air.local>
2024-01-26 23:43:42 +0530

    changes

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors);
-app.use(express.json()); 
+app.use(cors());
+//app.use(express.json()); 
 
 app.use(bodyParser.json());
 
